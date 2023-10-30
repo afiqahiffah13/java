f102d99ceb7165503409ef633c7e72e941e3c4e0
iffah <afiqahiffah1307@gamil.com>
2023-10-30 09:51:09 +0800

    use scanner

---
+++ src/userInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) {
+
+        Scanner iffah = new Scanner(System.in);
+        System.out.println(iffah.nextLine());
+        
+    } 
+
+}
