89d07f47cd15df0baccdf1d2014662d79177cb7a
iffah <afiqahiffah1307@gamil.com>
2023-10-30 09:39:30 +0800

    variable

---
+++ src/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        double sardin;
+        sardin = 7.50;
+        System.out.print("saya nak beli sardin harga ");
+        System.out.print(sardin);
+
+    }
+}
+
