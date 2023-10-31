a10bde9bc1ad570c19ed55e2de8023ad9659fa00
iffah <afiqahiffah1307@gamil.com>
2023-10-31 09:43:53 +0800

    menaik

---
+++ src/menaik.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class menaik {
+     public static void main(String[] args) {
+            
+            Scanner jef = new Scanner(System.in);        
+            int masa = 5;
+            ++masa;
+    
+            System.out.println(masa);
+    
+    
+        }
+        
+    }
+    
+    
+
