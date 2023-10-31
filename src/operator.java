929e00d490a982cadba2547a35ad972fa5429e51
iffah <afiqahiffah1307@gamil.com>
2023-10-31 09:20:22 +0800

    operator

---
+++ src/operator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class operator {
+    public static void main(String[] args) {
+        
+        Scanner tom = new Scanner(System.in);
+        int lelaki,perempuan,kawan;
+        lelaki = 17;
+        perempuan = 9;
+        kawan = lelaki % perempuan;
+    
+
+        System.out.println(kawan);
+
+    }
+
+
+
+}
