c30529c82c4449ec15e53e7fc907ead91ca31245
iffah <afiqahiffah1307@gamil.com>
2023-11-03 16:51:10 +0800

    ifStatement

---
+++ src/ifstatement.java
@@ -0,0 +1,14 @@
+public class ifstatement {
+    public static void main(String[] args) {
+        int markah = 80;
+
+        if (markah >= 80){
+            System.out.println("Saya dapat A");
+        }else{ 
+            System.out.println("Saya dapat B");
+
+        }
+    }
+}
+
+
