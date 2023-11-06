66bf0b273d323a83872b7412e3714d6eb12da28d
iffah <afiqahiffah1307@gamil.com>
2023-11-06 16:10:24 +0800

    logicalOperator

---
+++ src/logicalOperator.java
@@ -0,0 +1,12 @@
+public class logicalOperator {
+    public static void main(String[] args) {
+        int perempuan = 23;
+        int lelaki = 21;
+        if (lelaki > 16 || perempuan > 19){
+            System.out.println("you can enter");
+        }else{
+            System.out.println("under age");
+        }
+    }
+
+}
