45b7fb8b0ca0f07fa9cc56dc22c11eeab0ac0a80
iffah <afiqahiffah1307@gamil.com>
2023-11-06 15:55:06 +0800

    kiraanSepuluh

---
+++ src/kiraanSepuluh.java
@@ -0,0 +1,13 @@
+public class kiraanSepuluh {
+    public static void main(String[] args) {
+
+        int number = 0; 
+        while (number <=10)
+        {
+            System.out.println(number);
+            ++number;
+        }
+        
+    }
+    
+}
