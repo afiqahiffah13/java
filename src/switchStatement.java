8b03bc737f6585b232986adc39333b0f5d3867a4
iffah <afiqahiffah1307@gamil.com>
2023-11-08 09:22:21 +0800

    switchStatement

---
+++ src/switchStatement.java
@@ -0,0 +1,18 @@
+public class switchStatement {
+  public static void main(String[] args) {
+    
+    int umur = 2;
+
+    switch(umur) {
+        case 1:
+           System.out.println("adik boleh meniarap");
+           break;
+        case 2:
+           System.out.println("adik boleh berdiri");
+           break;
+        case 3:
+           System.out.println("adik boleh berjalan");
+           
+    }
+  }  
+}
