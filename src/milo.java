d1a561413dad4f275327e35e2df0e2ae363a7b12
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-09 12:19:50 +0800

    methodSimpleMassage

---
+++ src/milo.java
@@ -0,0 +1,6 @@
+public class milo{
+    public void simpleMessage (String name){
+        System.out.println ("Hello " + name);
+
+    }
+}
---
+++ src/nasiLemak.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class nasiLemak {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+        milo manis = new milo ();
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        manis.simpleMessage(name);
+        
+
+
+    
+    
+    }
+}
