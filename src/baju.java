21f42f7d2b10813d90da5137e5bdef0cfec3c29f
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-14 09:42:06 +0800

    else if statement

---
+++ src/baju.java
@@ -0,0 +1,16 @@
+public class baju{
+    public static void main(String[] args) {
+        int age = 35;
+
+        if (age >= 60) {
+            System.out.println("You will get 30% discount");
+        }else if (age >= 50) {
+            System.out.println("You will get 25% discount");
+        }else if (age >= 40) {
+            System.out.println("You will get 20% discount");
+        }else {
+            System.out.println("You will get 15% discount");
+
+    }
+  }
+}
