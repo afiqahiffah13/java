95a07dacbda1e2a07f0b20aeb71f73e104601f52
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-14 09:57:15 +0800

    conditional operator

---
+++ src/kiwi.java
@@ -0,0 +1,7 @@
+public class kiwi {
+    public static void main(String[] args) {
+        int age = 45;
+        System.out.println(age > 50 ? "You are senior citizen" : "You are young");
+
+    }
+}
