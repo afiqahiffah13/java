e49f36f09eca4b0998e4b87598733b72cb973972
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-15 10:00:36 +0800

    for loop

---
+++ src/seluar.java
@@ -0,0 +1,7 @@
+public class seluar {
+    public static void main (String[] args) {
+        for (int counter = 3; counter <=21; counter +=3) {
+            System.out.println(counter);
+        }
+    }
+}
