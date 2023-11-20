8fec9e015339a82f5fe0be47078eae8e846810bd
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-20 09:39:40 +0800

    square root (punca kuasa)

---
+++ src/sudu.java
@@ -0,0 +1,20 @@
+public class sudu {
+    public static void main (String[] args){
+        System.out.println(Math.sqrt(9));
+
+    }
+}
+
+// System.out.println(Math.abs(-2.5)); (positif number negatif)
+
+// System.out.println(Math.max(9.6,5.4)); (the max num)
+
+// System.out.println(Math.min(10.8,3.2)); (the min num)
+
+// System.out.println(Math.ceil(6.6)); (bundarkan)
+
+// System.out.println(Math.floor(8.9)); (ambil number hadapan = int )
+
+// System.out.println(Math.pow(5,3)); (5*5*5)
+
+// System.out.println(Math.sqrt(9)); (3)
