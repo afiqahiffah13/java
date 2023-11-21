a4d9c4d26e9d4df222268d6537fbee93881667d0
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-21 15:25:45 +0800

    simple array

---
+++ src/sabun.java
@@ -0,0 +1,7 @@
+public class sabun {
+    public static void main (String[] args){
+        int adik[] = {1,4,5,7};
+        System.out.println(adik[2]);
+    }
+}
+// int adik[] = {1,4,5,7} (adik[2]) masukkan nombor dalam braket berdasarkan {}
