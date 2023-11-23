e5aec1b3dad37e4cc7652112a919b724e6ee7183
afiqahiffah13 <afiqahiffah1307@gmail.com>
2023-11-23 11:22:00 +0800

    enhanced loop

---
+++ src/jep.java
@@ -0,0 +1,15 @@
+
+//pembelajaran: Enhanced Loop
+
+public class jep{
+    public static void main(String[] args){
+        int lala[] = {45, 67, 89, 39, 20};
+        int sum=0;
+
+        for(int x:lala){  //identifier   // x data type
+            sum += x;
+        }
+        System.out.println("Total of the sum is: " +sum);
+    }
+}
+
